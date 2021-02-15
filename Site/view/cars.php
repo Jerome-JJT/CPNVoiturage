b31576393f10c0d6f446baacd1a78e2958f10063
jeromejj <jerome.jaquemet@gmail.com>
2021-02-15 14:35:29 +0100

    Ajout de css responsive pour les onglets

--- Site/view/cars.php
+++ Site/view/cars.php
@@ -6,35 +6,50 @@ ob_start();
 ?>
 
 <div class="navi">
-  <div class="nav-tab">
-    <h3>Lundi</h3>
+
+  <div style="display:inline-block">
+    <div class="day-select">
+      <div class="day-tab">
+        <strong>Jour</strong>
+      </div>
+
+      <div class="other-days">
+        <div class="nav-tab">
+          <strong>Lundi</strong>
+        </div>
+
+        <div class="nav-tab">
+          <strong>Mardi</strong>
+        </div>
+
+        <div class="nav-tab">
+          <strong>Mercredi</strong>
+        </div>
+
+        <div class="nav-tab">
+          <strong>Jeudi</strong>
+        </div>
+
+        <div class="nav-tab">
+          <strong>Vendredi</strong>
+        </div>
+      </div>
+    </div>
   </div>
 
-  <div class="nav-tab">
-    <h3>Mardi</h3>
-  </div>
-
-  <div class="nav-tab">
-    <h3>Mercredi</h3>
-  </div>
-
-  <div class="nav-tab">
-    <h3>Jeudi</h3>
-  </div>
-
-  <div class="nav-tab">
-    <h3>Vendredi</h3>
-  </div>
 
   <button><strong>Profil</strong></button>
+
 
 </div>
 
 <div id="monday" class="carDay">
-  <div style="margin: 0 20px; height: 300px;">
-    <div style="text-align: center; height: 15%; font-size: 20px"><strong>Aller</strong></div>
+  <div class="travelDisplay">
+    <div style="text-align: center; height: 15%; font-size: 20px">
+      <strong>Aller</strong>
+    </div>
 
-    <div style="width: 35%; height: 85%; display: inline-block; vertical-align: top; background-color: magenta">
+    <div class="travelCase" style="width: 35%; background-color: magenta">
       <strong>Ajout de voiture</strong><br><br>
 
       <label for="nbPlaces">Nombre de places passagers</label>
@@ -43,43 +58,43 @@ ob_start();
       <button>Ajouter ma voiture</button>
     </div
 
-    ><div style="background-color: cyan; width: 65%; display: inline-block; vertical-align: top; height: 85%; overflow: scroll">
-      <table style="border-collapse: collapse; margin: 5px; text-align: center">
+    ><div class="travelCase" style="background-color: cyan; width: 65%; overflow: scroll">
+      <table class="carTable">
         <tr>
-          <th style="border: 1px solid black">Conducteur</th>
-          <th style="border: 1px solid black">Seuls</th>
-          <th style="border: 1px solid black">DWZ</th>
+          <th>Conducteur</th>
+          <th>Seuls</th>
+          <th>DWZ</th>
 
         </tr>
 
         <tr>
-          <th style="border: 1px solid black">Ville</th>
-          <td style="border: 1px solid black">───</td>
-          <td style="border: 1px solid black">p</td>
+          <th>Ville</th>
+          <td>───</td>
+          <td>p</td>
         </tr>
 
         <tr>
-          <th style="border: 1px solid black">Heure de départ</th>
-          <td style="border: 1px solid black">───</td>
-          <td style="border: 1px solid black">p</td>
+          <th>Heure de départ</th>
+          <td>───</td>
+          <td>p</td>
         </tr>
 
         <tr>
-          <th style="border: 1px solid black">Heure d'arrivée</th>
-          <td style="border: 1px solid black">───</td>
-          <td style="border: 1px solid black">p</td>
+          <th>Heure d'arrivée</th>
+          <td>───</td>
+          <td>p</td>
         </tr>
 
         <tr>
-          <th style="border: 1px solid black">Places restantes</th>
-          <td style="border: 1px solid black;">&infin;</td>
-          <td style="border: 1px solid black">p</td>
+          <th>Places restantes</th>
+          <td>&infin;</td>
+          <td>p</td>
         </tr>
 
         <tr>
-          <th style="border: 1px solid black">───</th>
-          <td style="border: 1px solid black;"><button>Quitter</button></td>
-          <td style="border: 1px solid black"><button>Rejoindre</button></td>
+          <th style="">───</th>
+          <td style=""><button>Quitter</button></td>
+          <td style=""><button>Rejoindre</button></td>
         </tr>
       </table>
     </div>
