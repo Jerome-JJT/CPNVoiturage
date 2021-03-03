8bd120ec4c3787f6772a3ccfa8e8681f16b30dbf
jeromejj <jerome.jaquemet@gmail.com>
2021-03-03 13:45:52 +0100

    Affichage dynamique des voitures

--- Site/view/cars.php
+++ Site/view/cars.php
@@ -6,6 +6,52 @@ $title = "Voitures - Lundi";
 $pageTitle = "Lundi";
 
 
+$cars = array(
+  "come" => array(
+    "solo" => array("ZYX", "WVO", "ASD", "KIU"),
+
+    "cars" => array(
+      array(
+        "conductor" => "DWZ",
+        "place" => "Chavo",
+        "departHour" => "8h00",
+        "arrivalHour" => "9h00",
+        "remainingPlaces" => "5",
+        "passengers" => array(
+          "ABC", "DEF", "GHI", "JKM", "LPO"
+        )
+      ),
+      array(
+        "conductor" => "MWA",
+        "place" => "Laus",
+        "departHour" => "10h00",
+        "arrivalHour" => "11h00",
+        "remainingPlaces" => "2",
+        "passengers" => array(
+          "ABC", "DEF"
+        )
+      )
+    ),
+  ),
+  "back" => array(
+    "solo" => array(),
+
+    "cars" => array(
+      array(
+        "conductor" => "DWZ",
+        "place" => "Chavo",
+        "departHour" => "16h00",
+        "arrivalHour" => "17h00",
+        "remainingPlaces" => "5",
+        "passengers" => array(
+          "ABC", "DEF"
+        )
+      )
+    )
+  )
+
+);
+
 ?>
 
 
@@ -26,41 +72,81 @@ $pageTitle = "Lundi";
 
   ><div class="travelCase" style="background-color: cyan; width: 65%; overflow: scroll">
     <table class="carTable">
-      <tr>
+      <tr style="background-color:white">
         <th>Conducteur</th>
         <th>Seuls</th>
-        <th>DWZ</th>
 
+        <?php foreach($cars["come"]["cars"] as $car): ?>
+          <th><?= $car["conductor"] ?></th>
+        <?php endforeach ?>
       </tr>
 
-      <tr>
+      <tr style="background-color:lightgray">
         <th>Ville</th>
         <td>───</td>
-        <td>p</td>
+
+        <?php foreach($cars["come"]["cars"] as $car): ?>
+          <td><?= $car["place"] ?></td>
+        <?php endforeach ?>
       </tr>
 
-      <tr>
+      <tr style="background-color:white">
         <th>Heure de départ</th>
         <td>───</td>
-        <td>p</td>
+
+        <?php foreach($cars["come"]["cars"] as $car): ?>
+          <td><?= $car["departHour"] ?></td>
+        <?php endforeach ?>
       </tr>
 
-      <tr>
+      <tr style="background-color:lightgray">
         <th>Heure d'arrivée</th>
         <td>───</td>
-        <td>p</td>
+
+        <?php foreach($cars["come"]["cars"] as $car): ?>
+          <td><?= $car["arrivalHour"] ?></td>
+        <?php endforeach ?>
       </tr>
 
-      <tr>
+      <tr style="background-color:white">
         <th>Places restantes</th>
         <td>&infin;</td>
-        <td>p</td>
+
+        <?php foreach($cars["come"]["cars"] as $car): ?>
+          <td><?= $car["remainingPlaces"] ?></td>
+        <?php endforeach ?>
       </tr>
 
-      <tr>
+
+      <?php $maxLines = count($cars["come"]["solo"]); ?>
+
+      <?php foreach($cars["come"]["cars"] as $car) {
+        $maxLines = (count($car["passengers"]) > $maxLines) ? count($car["passengers"]) : $maxLines;
+      } ?>
+
+
+      <?php for($i = 0; $i < $maxLines; $i++): ?>
+        <tr style="background-color:<?= $i%2==0 ? 'lightgray' : 'white'?>">
+          <td><?= $i+1 ?></td>
+
+          <td><?= $car["solo"][$i] ?></td>
+
+          <?php foreach($cars["come"]["cars"] as $car): ?>
+            <td><?= $car["passengers"][$i] ?></td>
+          <?php endforeach ?>
+        </tr>
+      <?php endfor ?>
+
+
+
+
+      <tr style="background-color:<?= $maxLines%2==0 ? 'lightgray' : 'white'?>">
         <th style="">───</th>
         <td style=""><button class="mybutton">Quitter</button></td>
-        <td style=""><button class="mybutton">Rejoindre</button></td>
+
+        <?php foreach($cars["come"]["cars"] as $car): ?>
+          <td style=""><button class="mybutton">Rejoindre</button></td>
+        <?php endforeach ?>
       </tr>
     </table>
   </div>
